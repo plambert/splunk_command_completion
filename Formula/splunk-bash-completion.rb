ea15d1700a34209e7ee7b81005ec686de6e62ecf
Paul M. Lambert <plambert@plambert.net>
2019-10-03 16:59:32 -0700

    add 1.0.2 checksum to homebrew formula

--- Formula/splunk-bash-completion.rb
+++ Formula/splunk-bash-completion.rb
@@ -1,9 +1,8 @@
 class SplunkBashCompletion < Formula
   desc "Completion for Atom Package Manager"
   homepage "https://github.com/plambert/splunk_command_completion"
-  url "https://github.com/plambert/splunk_command_completion/archive/v1.0.1.tar.gz"
-  # url "https://github.com/vigo/apm-bash-completion/archive/1.0.0.tar.gz"
-  sha256 "533e3876d838bedbc4e5eaac6ff3c3b5ec214779b0b0b5145cdb61deab670c9c"
+  url "https://github.com/plambert/splunk_command_completion/archive/v1.0.2.tar.gz"
+  sha256 "4e4d5ab517978f096d0ed1b84525b124fafecd4567478db19b882de45413ce6a"
 
   bottle :unneeded
 
